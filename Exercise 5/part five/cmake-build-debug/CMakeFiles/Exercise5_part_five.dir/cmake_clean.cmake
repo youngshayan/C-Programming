5e4bded2dbbeda01e448ae02892afc85729ddf9d
YOUNGSHAYAN <92734199+youngshayan@users.noreply.github.com>
2021-11-24 20:47:33 +0330

    Add files via upload

---
+++ Exercise 5/part five/cmake-build-debug/CMakeFiles/Exercise5_part_five.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercise5_part_five.dir/main.c.obj"
+  "Exercise5_part_five.exe"
+  "Exercise5_part_five.exe.manifest"
+  "Exercise5_part_five.pdb"
+  "libExercise5_part_five.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Exercise5_part_five.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
