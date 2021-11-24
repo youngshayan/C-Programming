b616e4ccec539ad598f894bc98ad69769febbb8a
YOUNGSHAYAN <92734199+youngshayan@users.noreply.github.com>
2021-11-24 17:36:16 +0330

    Add files via upload

---
+++ Exercise 4/Part two/cmake-build-debug/CMakeFiles/Exercise4_part_two.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercise4_part_two.dir/main.c.obj"
+  "Exercise4_part_two.exe"
+  "Exercise4_part_two.exe.manifest"
+  "Exercise4_part_two.pdb"
+  "libExercise4_part_two.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Exercise4_part_two.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
